5e2a9048efba59badd8995c5d6421baca910c735
AriaMahlouji <ariamahlooji.de@gmail.com>
2023-01-29 17:12:15 +0100

    Code Cleaning

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
-import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
 import './App.css';
 import SearchCard from './components/CourseGoals/SearchCard/SearchCard';
 
 const App = () => {
-  const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
-  ]);
-
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
-      return updatedGoals;
-    });
-  };
-
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
-      return updatedGoals;
-    });
-  };
-
-  let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
-  );
-
-  if (courseGoals.length > 0) {
-    content = (
-      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
-    );
-  }
 
   return (
     <div>
@@ -51,7 +20,7 @@ const App = () => {
 
       <div className='search-card-container'>
         <section id="search-card">
-          <SearchCard onAddGoal={addGoalHandler} />
+          <SearchCard/>
         </section>
       </div>
 
